a7511a78646dc537ea36cb106dc5c8d6b5e30389
terence <99847498+julius507@users.noreply.github.com>
2022-09-30 17:52:51 +0800

    Delete r281imei.sh

--- r281imei.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "Restoring IMEI..."
-ubus call version set_atcmd_info '{"atcmd":"AT*PROD=2"}' > /dev/null 2&>1
-ubus call version set_atcmd_info '{"atcmd":"AT*MRD_IMEI=D"}' > /dev/null 2&>1
-ubus call version set_atcmd_info '{"atcmd":"AT*MRD_IMEI=W,0101,01JAN1970,352075060055098"}' > /dev/null 2&>1
-ubus call version set_atcmd_info '{"atcmd":"AT*PROD=0"}' > /dev/null 2&>1
-echo "Done! Modem is rebooting..."
-ubus call version set_atcmd_info '{"atcmd":"AT+RSTSET"}' > /dev/null 2&>1
-
